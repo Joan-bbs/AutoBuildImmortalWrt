f75fc33de47d61894ea7bcb62fbba9ca5652b593
Joan-bbs <liuxing0726@gmail.com>
2025-08-08 14:23:38 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -51,11 +51,11 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 
 # ============= imm仓库外的第三方插件==============
 # ============= 若启用 则打开注释 ================
@@ -76,7 +76,7 @@ PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 #PACKAGES="$PACKAGES luci-app-partexp"
 #PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn"
 # 酷猫主题 by sirpdboy 
-#PACKAGES="$PACKAGES luci-theme-kucat"
+PACKAGES="$PACKAGES luci-theme-kucat"
 # 网络测速 by sirpdboy 
 #PACKAGES="$PACKAGES luci-app-netspeedtest"
 #PACKAGES="$PACKAGES luci-i18n-netspeedtest-zh-cn"
